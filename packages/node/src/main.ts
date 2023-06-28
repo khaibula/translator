e1c697d672909fbfc6eb6419e5b372cde0705629
khaibula <gaziev.777@bk.ru>
2023-06-28 11:25:25 +0300

    add speech to text

--- packages/node/src/main.ts
+++ packages/node/src/main.ts
@@ -1,11 +1,24 @@
 import fs from 'fs';
 import ffmpeg from 'fluent-ffmpeg';
 import path from 'path';
+import {Configuration, OpenAIApi} from "openai";
+
+const generateAudioPath = () => {
+  let addNumCount = 0;
+  let readNumCount = 0;
+
+  return {
+    addFile: () => `audio_part_${++addNumCount}.mp3`,
+    readFile: () => `audio_part_${++readNumCount}.mp3`,
+  }
+}
+
+const { addFile, readFile } = generateAudioPath();
+
 
 function videoToAudioChunks(videoPath, outputDir, chunkSizeMB = 4) {
   return new Promise((resolve, reject) => {
     const chunkSize = chunkSizeMB * 1024 * 1024;  // convert MB to bytes
-    let partNum = 0;
     let byteCount = 0;
     let writeStream;
 
@@ -20,8 +33,9 @@ function videoToAudioChunks(videoPath, outputDir, chunkSizeMB = 4) {
           // Start a new file
           if (writeStream) {
             writeStream.end();
+            audioChunksToText(readFile());
           }
-          writeStream = fs.createWriteStream(path.join(outputDir, `audio_part_${++partNum}.mp3`));
+          writeStream = fs.createWriteStream(path.join(outputDir, addFile()));
           byteCount = 0;
         }
 
@@ -36,8 +50,29 @@ function videoToAudioChunks(videoPath, outputDir, chunkSizeMB = 4) {
   });
 }
 
-videoToAudioChunks('./index.mp4', './output')
-  .then((res) => {
-    console.log('res', res)
-  })
+async function audioChunksToText(filePath: string) {
+  try {
+    const configuration = new Configuration({
+      organization: "org-wifCiRmsXjUtw09PqWxLVuhH",
+      apiKey: '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',
+    });
+
+    const fileUrl = path.resolve(`./output/${filePath}`);
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createTranscription(
+      fs.createReadStream(fileUrl) as any,
+      "whisper-1",
+      undefined,
+      'text',
+      undefined,
+      'ru',
+    );
+
+    console.log('response', response.data);
+  } catch (e) {
+    console.log('Error is ', e);
+  }
+}
+
+videoToAudioChunks(path.resolve('./assets/index.mp4'), path.resolve('./output'))
   .catch(console.error);
